eb125d94c27ee1949324c7a333bbf6bfcaba46cc
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2022-03-07 09:18:04 +0100

    Add files via upload

---
+++ Unchained/src/main/java/com/unchained/Unchained/UnchainedApplication.java
@@ -0,0 +1,13 @@
+package com.unchained.Unchained;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnchainedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnchainedApplication.class, args);
+	}
+
+}
---
+++ Unchained/src/test/java/com/unchained/Unchained/UnchainedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unchained.Unchained;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnchainedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
