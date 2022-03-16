2b2a81c27856d282e85f5a73f08b6ceb8d4f25d9
joosn <nico.joos@students.fhnw.ch>
2022-03-16 11:55:34 +0100

    Product Endpoint

--- Unchained/src/main/java/com/unchained/Unchained/Service/ProductService.java
+++ Unchained/src/main/java/com/unchained/Unchained/Service/ProductService.java
@@ -12,7 +12,7 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    private Product (Product product) {
+    private Product saveProduct(Product product) {
         return productRepository.save(product);
     }
 }
