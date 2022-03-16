413163e6ef7e7b9256981dc77e78d19afa0d2d53
joosn <nico.joos@students.fhnw.ch>
2022-03-16 11:54:59 +0100

    Redesign

---
+++ Unchained/src/main/java/com/unchained/Unchained/Service/ProductService.java
@@ -0,0 +1,18 @@
+package com.unchained.Unchained.Service;
+
+
+import com.unchained.Unchained.Data.Domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.unchained.Unchained.Data.Repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Product (Product product) {
+        return productRepository.save(product);
+    }
+}
--- Unchained/src/main/java/com/unchained/Unchained/api/ProductEndpoint.java
+++ Unchained/src/main/java/com/unchained/Unchained/api/ProductEndpoint.java
@@ -1,6 +1,36 @@
 package com.unchained.Unchained.api;
 
+import com.unchained.Unchained.Data.Domain.Product;
+import com.unchained.Unchained.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path ="/api")
 public class ProductEndpoint {
 
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping(path = "/product,", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Product> postProduct(@RequestBody Product product){
+
+        try {
+            productService.saveProduct(product);
+        } catch (Exception e) {
+
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{productId}").buildAndExpand(product.getProductId()).toUri();
+
+        return ResponseEntity.created(location).body(product);
+    }
 
 }
