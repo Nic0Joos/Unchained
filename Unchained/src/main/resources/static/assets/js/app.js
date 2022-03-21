dd86831af5d3faf6ffdddc3536702999aef2133c
joosn <nico.joos@students.fhnw.ch>
2022-03-21 14:31:25 +0100

    Implemented login

--- Unchained/src/main/resources/static/assets/js/app.js
+++ Unchained/src/main/resources/static/assets/js/app.js
@@ -25,3 +25,26 @@ function register(name, street, ZIPCode, city, email, password, callbackSuccess,
         }
     });
 }
+
+function login(email, password, remember, callback) {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+            "X-XSRF-TOKEN": getCookie("XSRF-TOKEN")
+        },
+        url: serviceEndpointURL + "/login",
+        data: JSON.stringify({
+            "email": email,
+            "password": password,
+            "remember": remember
+        }),
+        success: function (data, textStatus, response) {
+            callback(true);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+            callback(false);
+        }
+    });
+}
