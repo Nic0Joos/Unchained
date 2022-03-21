6e25786c8252670e445b06297aaa9da5fc834d92
joosn <nico.joos@students.fhnw.ch>
2022-03-21 14:01:10 +0100

    Implemented Register

---
+++ Unchained/src/main/resources/static/assets/js/app.js
@@ -0,0 +1,27 @@
+
+
+function register(name, street, ZIPCode, city, email, password, callbackSuccess, callbackError) {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+            "X-XSRF-TOKEN": getCookie("XSRF-TOKEN")
+        },
+        url: serviceEndpointURL + "/user/register",
+        data: JSON.stringfy({
+            "name": name,
+            "street": street,
+            "city": city,
+            "ZIPCode": ZIPCode,
+            "email": email,
+            "password": password
+        }),
+        success: function (data, textStatus, response) {
+            callbackSuccess(true);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+            callbackError(jqXHR.responseJSON.message);
+        }
+    });
+}
