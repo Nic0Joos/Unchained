de7f6e10373d37030be11d0f560656f117ae35ae
joosn <nico.joos@students.fhnw.ch>
2022-05-10 23:51:43 +0200

    added app.json

--- Unchained/src/main/java/com/unchained/Unchained/Service/LoggerService.java
+++ Unchained/src/main/java/com/unchained/Unchained/Service/LoggerService.java
@@ -2,6 +2,8 @@ package com.unchained.Unchained.Service;
 
 import org.springframework.stereotype.Service;
 
+//Created with the help of: https://jenkov.com/tutorials/java-logging/handlers.html
+
 @Service
 public class LoggerService {
 
