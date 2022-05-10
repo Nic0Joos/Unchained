9ac1a233bba53f42af5a7a5168f99374daf75591
joosn <nico.joos@students.fhnw.ch>
2022-05-10 17:23:56 +0200

    Adapted CostRequestEndpoint

--- Unchained/src/main/java/com/unchained/Unchained/Controller/CostRequestEndpoint.java
+++ Unchained/src/main/java/com/unchained/Unchained/Controller/CostRequestEndpoint.java
@@ -2,6 +2,7 @@ package com.unchained.Unchained.Controller;
 
 import com.unchained.Unchained.Data.Domain.CostRequest;
 import com.unchained.Unchained.Service.CostRequestService;
+import com.unchained.Unchained.Service.UserDetailsServiceImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,15 @@ public class CostRequestEndpoint {
     @Autowired
     private CostRequestService costRequestService;
 
+    @Autowired
+    private UserDetailsServiceImp userDetailsServiceImp;
+
     Logger logger = LoggerFactory.getLogger(CostRequestEndpoint.class);
 
     @GetMapping(path="/costrequest")
     public @ResponseBody CostRequest getShippingCosts(@RequestBody CostRequest costRequest){
         try {
-            costRequest.setPrice(costRequestService.getCosts(costRequest.getPallets(), costRequest.getKm()));
+            costRequest.setPrice(costRequestService.getCosts(costRequest.getPallets(), userDetailsServiceImp.getCurrentUser().getTraveldistance()));
         } catch (Exception e) {
             logger.error(e.toString());
         }
