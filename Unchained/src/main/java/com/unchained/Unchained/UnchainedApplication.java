d9bc4c8aae7a9b317f4f1bc3213543f47d0a4ca4
joosn <nico.joos@students.fhnw.ch>
2022-05-16 21:56:18 +0200

    bugfix

--- Unchained/src/main/java/com/unchained/Unchained/UnchainedApplication.java
+++ Unchained/src/main/java/com/unchained/Unchained/UnchainedApplication.java
@@ -2,7 +2,6 @@ package com.unchained.Unchained;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication()
