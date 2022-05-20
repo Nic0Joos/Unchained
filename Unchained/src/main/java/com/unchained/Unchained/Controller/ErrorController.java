fd57871ee4d42792619cbaf6fdabefa161c89111
joosn <nico.joos@students.fhnw.ch>
2022-05-20 15:40:20 +0200

    ErrorController

--- Unchained/src/main/java/com/unchained/Unchained/Controller/ErrorController.java
+++ Unchained/src/main/java/com/unchained/Unchained/Controller/ErrorController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//Author: Lennart
+//Author: Luca
 @Controller
-@RequestMapping(path = "/error")
 public class ErrorController {
 
     @GetMapping(path="/403")
